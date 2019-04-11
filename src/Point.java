de6c32831160254296ad005714fe15b8b02fb6fc
maxch <chowm21@greenhill.org>
2019-04-11 12:08:41 -0500

    hello

--- src/Point.java
+++ src/Point.java
@@ -10,45 +10,70 @@ public class Point {
         y = y1;
     }
 
-    public int GetX()
-    {
+    public int GetX() {
         return x;
     }
-    public int GetY()
-    {
+
+    public int GetY() {
         return y;
     }
+
     public void setLocation(int x1, int y1) {
         x = x1;
         y = y1;
     }
-    public String toString()
-    {
-        return ("Point = ("+x+","+y+").");
+
+    public String toString() {
+        return ("Point = (" + x + "," + y + ").");
     }
+
     public static double distance(Point p1, Point p2) {
-        double VarX = Math.pow(p1.x - p2.x,2);
-        double VarY = Math.pow(p1.y - p2.y,2);
+        double VarX = Math.pow(p1.x - p2.x, 2);
+        double VarY = Math.pow(p1.y - p2.y, 2);
         return Math.round(Math.sqrt(VarX + VarY));
     }
+
     public boolean isVertical(Point other) {
         if (x == other.x) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
     public double slope(Point other) {
-        return (other.y - y)/(other.x - x);
+        if (x == other.x) {
+            return -.0000001;
+        }
+        if (y == other.y) {
+            return (double)(other.y - y)/(other.x - x);
+        }
+        else {
+            return 0;
+        }
     }
+
     public int manhattanDistance(Point other) {
         int absX = Math.abs(other.x - x);
         int absY = Math.abs(other.y - y);
         int totalDistance = absX + absY;
         return totalDistance;
     }
-//    public boolean isCollinear(Point p1, Point p2) {
-//    }
+
+    public boolean isCollinear(Point p2, Point p3) {
+        boolean t1 = isVertical(p2);
+        boolean t2 = isVertical(p3);
+        if (t1 && t2) {
+            return true;
+        }
+        double s1 = slope(p2);
+        double s2 = slope(p3);
+        if (Math.abs(s1 - s2) <= .001) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
--- src/PointClient.java
+++ src/PointClient.java
@@ -7,11 +7,14 @@ public class PointClient {
     {
         Point p1 = new Point();
         Point p2 = new Point();
+        Point p3 = new Point();
 
-        p1.SetX(3);
-        p1.SetY(7);
+        p1.SetX(-0);
+        p1.SetY(5);
         p2.SetX(-2);
         p2.SetY(-1);
+        p3.SetX(10);
+        p3.SetY(-6);
 
         double distance = Point.distance(p1, p2);
 
@@ -23,5 +26,7 @@ public class PointClient {
         System.out.println(p1.slope(p2));
 
         System.out.println(p1.manhattanDistance(p2));
+        
+        System.out.println("The point is collinear: " + p1.isCollinear(p2,p3));
     }
 }
