89fa9655717f83479ddbf9816f2417de1c9cc4e9
maxch <chowm21@greenhill.org>
2019-04-21 11:54:26 -0500

    hello

--- src/box.java
+++ src/box.java
@@ -1,4 +1,7 @@
-import java.util.*;
+//Max Chow
+//This is where all the methods and variables are created.
+//Sets different methods, including: setting the dimensions, getting the dimensions, calculating the area,
+//and calculating the volume
 
 public class box{
     private int width;
@@ -26,6 +29,13 @@ public class box{
     }
 
     public int GetHieght() { return height; }
+
+    public void setDimensions(int width1, int length1, int height1) {
+        width = width1;
+        length = length1;
+        height = height1;
+    }
+
 
     public static double CalculateArea(box Width1, box Length1) {
         return Width1.width * Length1.length;
--- src/boxClass.java
+++ src/boxClass.java
@@ -1,21 +1,35 @@
+//Max Chow
+//This is where all the methods and dimensions are defined from the box class
+//Prints out the dimensions, area, and volume of both box1 and box2
+
 public class boxClass {
     public static void main(String[] args) {
+        //sets the dimensions of box1
         box box1 = new box();
-        box1.SetWidth(1);
-        box1.SetLength(2);
-        box1.SetHeight(3);
+        box1.SetWidth(2);
+        box1.SetLength(4);
+        box1.SetHeight(-3);
 
+        //prints out the dimensions of box1
+        System.out.println("The dimensions of box1[width:" + box1.GetWidth() + " length:" +
+                box1.GetLength() + " height:" + box1.GetHieght());
+
+        //sets the dimensions of box2
         box box2 = new box();
         box2.SetLength(1);
         box2.SetWidth(1);
         box2.SetHeight(1);
 
-        System.out.println("The area of box1: " + box1.CalculateArea(box1, box1));
+        //prints out the dimensions of box2
+        System.out.println("The dimensions of box2[width:" + box2.GetWidth() + " length:" +
+                box2.GetLength() + " height:" + box2.GetHieght());
 
+        //prints out the area and volume, respectively, of box1
+        System.out.println("The area of box1: " + box1.CalculateArea(box1, box1));
         System.out.println("The volume of box1: " + box1.CalculateVolume(box1, box1, box1));
 
+        //prints out the area and volume, respectively, of box2
         System.out.println("The area of box2: " + box2.CalculateArea(box2, box2));
-
         System.out.println("The volume of box2: " + box2.CalculateVolume(box2, box2, box2));
     }
 }
