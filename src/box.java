6632eb24bbb2e671bbed30bc125e2d4ffdf3edb2
maxch <chowm21@greenhill.org>
2019-04-21 11:56:29 -0500

    hello

--- src/box.java
+++ src/box.java
@@ -4,43 +4,37 @@
 //and calculating the volume
 
 public class box{
+    //sets variables
     private int width;
     private int length;
     private int height;
 
+    //allows for change on another variable and not the original
     public void SetWidth(int width1) {
         width = width1;
     }
-
     public void SetLength(int length1) {
         length = length1;
     }
-
     public void SetHeight(int height1) {
         height = height1;
     }
 
+    //These class is used to print out the dimensions of each bo
     public int GetWidth() {
         return width;
     }
-
     public int GetLength() {
         return length;
     }
-
     public int GetHieght() { return height; }
 
-    public void setDimensions(int width1, int length1, int height1) {
-        width = width1;
-        length = length1;
-        height = height1;
-    }
-
-
+    //Calculates the area of the given box
     public static double CalculateArea(box Width1, box Length1) {
         return Width1.width * Length1.length;
     }
 
+    //Calculates the volume of the chosen box
     public static double CalculateVolume(box Width1, box Length1, box Height1) {
         return Width1.width * Length1.length * Height1.height;
     }
